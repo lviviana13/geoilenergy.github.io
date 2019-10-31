1d9160d26edcb20e780a0e995b946780d377b276
Viviana Bautista <lviviana13@gmail.com>
2019-10-31 12:09:34 -0500

    Página de Servicio de Ecopia

--- resources/views/servicios/geoespacialesServ/ecopia.blade.php
+++ resources/views/servicios/geoespacialesServ/ecopia.blade.php
@@ -1,221 +1,160 @@
 @extends('layouts.small-header')
 @section('title'){{$title}}@endsection
 @section('content')
-    <!-- Contact -->
-    <section class="about-info  p-3">
-        <div class="content-w3ls-inn px-lg-5">
-            <div class="container py-3">
-                <div class="content-sing-w3pvt ">
-                <a href="https://www.ecopiatech.com/" target="_blank" >
-                    <h3 class="text-center"><img class="img-fluid" style="max-height: 150px" src="/images/solucionesGeomatica/servicios/index/ecopia.png" alt=""></h3> 
-                    </a> <br>
-                    
-
-                    <p> ECOPIA es un proveedor de productos cartográficos de alta definición (HD) a escala global que son Precisas, Escalables y Actualizadas. Utiliza Inteligencia Artificial (AI) y Machine Learning para convertir información de imágenes de alta resolución en mapas (HD), que representan la versión más precisa y actualizada de la superficie.</p>
-                    
+<!-- Contact -->
+<section class="about-info  p-3">
+    <div class="content-w3ls-inn px-lg-5">
+        <div class="container py-3">
+            <div class="content-sing-w3pvt ">
+                <a href="https://www.ecopiatech.com/" target="_blank">
+                    <h3 class="text-center"><img class="img-fluid" style="max-height: 150px" src="/images/solucionesGeomatica/servicios/index/ecopia.png" alt=""></h3>
+                </a> <br>
 
 
-                    <div class="row " id="features">
+                <p> ECOPIA es un proveedor de productos cartográficos de alta definición (HD) a escala global que son Precisas, Escalables y Actualizadas. Utiliza Inteligencia Artificial (AI) y Machine Learning para convertir información de imágenes de alta resolución en mapas (HD), que representan la versión más precisa y actualizada de la superficie.</p>
 
-<div class="col-md-4 feature-gird">
-            <div class="row features-hny-inner-gd">
-                <div class="col-md-3 featured_grid_left">
-                    <div class="icon_left_grid">
-                    <p class="text-center"><img class="img-fluid" style="max-height: 200px"  src="/images/software/fme/dos.png" alt=""></p> 
+
+
+                <div class="row about-w3pvt-top mt-3">
+                    <div class="col-lg-6 about-info">
+                        <h4 class="title-hny mb-3">LOS MEJORES MAPAS VECTORIALES EN HD COMIENZAN CON LAS MEJORES IMÁGENES.</h4>
+                        <p class="text-center"><img class="img-fluid" style="max-height: 200px" src="/images/solucionesGeomatica/servicios/ecopia/siete.png" alt=""></p>
+                        <P>Ecopia y GOE International han construido una sólida red global de socios de datos de imágenes geoespaciales, que ofrece acceso a las imágenes más actuales de nuestra Tierra tan pronto como esté disponible. </P>
+
+
                     </div>
-                </div>
-                <div class="col-md-9 featured_grid_right_info pl-lg-0">
-                    <h4>Preciso</h4>
-                    <p>Ecopia ha refinado el  proceso de extracción de mapas vectoriales HD a escala, manteniendo una precisión del 95%
-                </p>
-                    
-                </div>
-                
-            </div>
-        </div>
+                    <div class="col-lg-6 about-info">
+                        <table class="table">
 
-
-
-
-<div class="col-md-4 feature-gird">
-<div class="row features-hny-inner-gd">
-    <div class="col-md-3 featured_grid_left">
-        <div class="icon_left_grid">
-        <p class="text-center"><img class="img-fluid" style="max-height: 200px"  src="/images/software/fme/tres.png" alt=""></p> 
-        </div>
-    </div>
-    <div class="col-md-9 featured_grid_right_info pl-lg-0">
-        <h4>Escalable</h4>
-        <p>Ecopia produce millones de km2
-de mapas vectoriales HD por mes, ofreciendo una capacidad inigualable
-
-</p>
-        
-    </div>
-    
-</div>
-</div>
-
-<div class="col-md-4 feature-gird">
-<div class="row features-hny-inner-gd">
-    <div class="col-md-3 featured_grid_left">
-        <div class="icon_left_grid">
-        <p class="text-center"><img class="img-fluid" style="max-height: 200px"  src="/images/software/fme/cuatro.png" alt=""></p> 
-        </div>
-    </div>
-    <div class="col-md-9 featured_grid_right_info pl-lg-0">
-        <h4>Actualizable</h4>
-        <p>Ecopia cuenta con una amplia fuente de datos para producir mapas vectoriales en HD  que representan la versión mas actualizada de la realidad</p>
-        
-    </div>
-   
-</div>
-</div>
-
-
-
-                    <div class="row about-w3pvt-top mt-3">
-                        <div class="col-lg-6 about-info">
-                        <h4 class="title-hny mb-3">Características</h4>
-
-                        
-                        <p  class="text-center">  <iframe  width="350" height="285" src="https://www.youtube.com/embed/sn7K0xhiNSk" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe> </p>
-                            
-                   
-                            <table class="table">
-                           
                             <tbody>
-                            <tr>
-                            <td><img class="img-fluid" style="max-height: 500px" src="/images/software/fme/cinco.png" alt=""></td>
-                                
-                                <td> <strong>FME Desktop</strong> - Cree flujos de trabajo de transformación y conversión de datos repetibles mediante una interfaz intuitiva de arrastrar y soltar.
-                                </td>
-                            </tr>
-                            <tr>
-                            <td><img class="img-fluid" style="max-height: 500px" src="/images/software/fme/seis.png" alt=""></td>
-                                
-                                <td> <strong>FME Server</strong> - Automatice los flujos de trabajo integrados en FME Desktop y ejecútelos de forma programada o en respuesta a los desencadenantes y obtenga resultados lo antes posible.
-                                </td>
-                            </tr>
+                                <tr>
 
-                            <tr>
-                            <td><img class="img-fluid" style="max-height: 500px" src="/images/software/fme/siete.png" alt=""></td>
-                              
-                                <td> <strong>FME Cloud </strong>- Elija este modelo de pago por uso y déjenos encargarnos del alojamiento y la gestión de su servidor FME
-                                </td>
-                            </tr>
 
-                            
+                                    <td> <img class="img-fluid" style="max-height: 100px" src="/images/solucionesGeomatica/servicios/ecopia/cli.png" alt=""> <strong> Clientes Finales
+                                        </strong> <br> Los clientes finales se benefician de recibir la información más completa, precisa y actualizada para impulsar una mejor toma de decisiones.
+                                    </td>
+                                </tr>
+                                <tr>
 
-                          
+
+                                    <td> <img class="img-fluid" style="max-height: 100px" src="/images/solucionesGeomatica/servicios/ecopia/prov.png" alt=""> <strong> Proveedores de imagenes</strong> <br> Ofrece información contextual a gran escala y de alta precisión sobre una variedad de características que se distinguen de las imágenes geoespaciales.
+                                        Mapas de Clasificación de Tierras a gran escala, con alta precisión extraída de imágenes geoespaciales. Incluye múltiples capas tales como: vías, ferrocarriles, aeropuertos, bosques, zonas de pastos y sembrados, entre otros.
+                                    </td>
+                                </tr>
+
+                                <tr>
+
+
+                                    <td> <img class="img-fluid" style="max-height: 100px" src="/images/solucionesGeomatica/servicios/ecopia/ra.png" alt=""><strong>Revendedores de valor agregado</strong><br> A partir de la comparación de imágenes multi-temporales, se realiza una extracción de los elementos de la superficie que han cambiado a través del tiempo.
+                                    </td>
+                                </tr>
+
+
                             </tbody>
                         </table>
-
-
-                            
-                        </div>
-                        <div class="col-lg-6 about-info">
-
-                       
-
-                       <br>
-                            <h4 class="title-hny mb-3">Aspectos destacados</h4>
-
-                            <ul>
-                            <li  style="background-color: #ffffed;" class="list-group-item">
-                            
-                                <p>• 	Permite leer, editar y exportar datos en diferentes de formatos (Base de datos, archivos de texto, mapas)  <br>
-
-                                <p class="text-center"><img class="img-fluid" style="max-height: 200px"  src="/images/software/fme/uno.png" alt=""></p> 
-
-
-                                •	Automatización de procesos. ¡Aumenté su productividad! <br>
-                                •	Conecta datos y sistemas incompatibles. ¡Acceda toda su información! <br>
-                                •	Mantenga sincronizados los datos en toda la organización incluso grandes volúmenes de datos complejos y/o en tiempo real. <br>
-                                •	Envío de alertas cuando se requieran notificaciones (validación fallida, objetos entrando a una geofence o nueva información). <br>
-                                •	Incluye REST API y soporta protocolos comunes como email, SMS, WebSockets, JMS y muchos más. <br>
-                                •	Ayuda a disminuir tiempos de procesos y aumento de efectividad. ¡Valide y asegure la calidad de sus datos <br>
-
-                                                            
-                                </p>
-                            </li>
-                            
-                            </ul>
-                           
-                              
-                        </div>
                     </div>
+                </div>
 
-                    <div class="row " id="features">
+
+
+                <table class="table">
+                    <h4 class="title-hny mb-3">SERVICIOS</h4>
+                    <tbody>
+                        <tr>
+                            <td><img class="img-fluid" style="max-height: 300px" src="/images/solucionesGeomatica/servicios/ecopia/cuatro.png" alt=""></td>
+
+                            <td> <strong>EXTRACCIÓN MASIVA AUTOMATIZADA VECTORIAL DE CARACTERÍSTICAS
+                                    DEL TERRENO
+                                </strong> - Extracción de las características del terreno basada en vectores de los límites de predios/construcciones, cubriendo extensas áreas en poco tiempo, manteniendo una precisión a nivel humano y una capacidad de producción que supera los 15 millones de edificios por mes.
+                            </td>
+                        </tr>
+                        <tr>
+                            <td><img class="img-fluid" style="max-height: 300px" src="/images/solucionesGeomatica/servicios/ecopia/cinco.png" alt=""></td>
+
+                            <td> <strong>CLASIFICACIÓN DE ELEMENTOS DEL TERRENO</strong> - Ofrece información contextual a gran escala y de alta precisión sobre una variedad de características que se distinguen de las imágenes geoespaciales.
+                                Mapas de Clasificación de Tierras a gran escala, con alta precisión extraída de imágenes geoespaciales. Incluye múltiples capas tales como: vías, ferrocarriles, aeropuertos, bosques, zonas de pastos y sembrados, entre otros.
+                            </td>
+                        </tr>
+
+                        <tr>
+                            <td><img class="img-fluid" style="max-height: 300px" src="/images/solucionesGeomatica/servicios/ecopia/seis.png" alt=""></td>
+
+                            <td> <strong>DETECCIÓN DE CAMBIOS EN EL TERRENO </strong>- A partir de la comparación de imágenes multi-temporales, se realiza una extracción de los elementos de la superficie que han cambiado a través del tiempo.
+                            </td>
+                        </tr>
+
+
+
+
+                    </tbody>
+                </table>
+                <hr>
+
+                <div class="row " id="features">
 
                     <div class="col-md-4 feature-gird">
-                                <div class="row features-hny-inner-gd">
-                                    <div class="col-md-3 featured_grid_left">
-                                        <div class="icon_left_grid">
-                                        <p class="text-center"><img class="img-fluid" style="max-height: 200px"  src="/images/software/fme/dos.png" alt=""></p> 
-                                        </div>
-                                    </div>
-                                    <div class="col-md-9 featured_grid_right_info pl-lg-0">
-                                        <h4>Conecte sus datos y aplicaciones</h4>
-                                        <p>
-
-                                    •	Conéctese a más de 450 formatos y aplicaciones <br>
-                                    •	Elimine los silos de datos para tomar decisiones de negocio autorizadas <br>
-                                    •	Personalice las conexiones con API, R o Python para añadir extensibilidad. <br>
-                                    </p>
-                                        
-                                    </div>
-                                    
+                        <div class="row features-hny-inner-gd">
+                            <div class="col-md-3 featured_grid_left">
+                                <div class="icon_left_grid">
+                                    <p class="text-center"><img class="img-fluid" style="max-height: 200px" src="/images/solucionesGeomatica/servicios/ecopia/uno.PNG" alt=""></p>
                                 </div>
                             </div>
+                            <div class="col-md-9 featured_grid_right_info pl-lg-0">
+                                <h4>Preciso</h4>
+                                <p>Ecopia ha refinado el proceso de extracción de mapas vectoriales HD a escala, manteniendo una precisión del 95%
+                                </p>
 
-
-              
-               
-                <div class="col-md-4 feature-gird">
-                    <div class="row features-hny-inner-gd">
-                        <div class="col-md-3 featured_grid_left">
-                            <div class="icon_left_grid">
-                            <p class="text-center"><img class="img-fluid" style="max-height: 200px"  src="/images/software/fme/tres.png" alt=""></p> 
                             </div>
-                        </div>
-                        <div class="col-md-9 featured_grid_right_info pl-lg-0">
-                            <h4>Transforme sus datos</h4>
-                            <p>Manipule sus modelos de datos su contenido para garantizar que dispone exactamente de lo que necesita
-</p>
-                            
-                        </div>
-                        
-                    </div>
-                </div>
 
-                <div class="col-md-4 feature-gird">
-                    <div class="row features-hny-inner-gd">
-                        <div class="col-md-3 featured_grid_left">
-                            <div class="icon_left_grid">
-                            <p class="text-center"><img class="img-fluid" style="max-height: 200px"  src="/images/software/fme/cuatro.png" alt=""></p> 
+                        </div>
+                    </div>
+
+
+
+
+                    <div class="col-md-4 feature-gird">
+                        <div class="row features-hny-inner-gd">
+                            <div class="col-md-3 featured_grid_left">
+                                <div class="icon_left_grid">
+                                    <p class="text-center"><img class="img-fluid" style="max-height: 200px" src="/images/solucionesGeomatica/servicios/ecopia/dos.PNG" alt=""></p>
+                                </div>
                             </div>
-                        </div>
-                        <div class="col-md-9 featured_grid_right_info pl-lg-0">
-                            <h4>Automatice sus flujos de trabajo</h4>
-                            <p>
+                            <div class="col-md-9 featured_grid_right_info pl-lg-0">
+                                <h4>Escalable</h4>
+                                <p>Ecopia produce millones de km2
+                                    de mapas vectoriales HD por mes, ofreciendo una capacidad inigualable
 
-•	Ejecute flujos de trabajo basados en eventos de forma automática para integrarlos en tiempo real. <br>
-•	Cree programas que envíen sus datos actualizados cuando y donde sea necesario. <br>
-•	Elimine el esfuerzo manual de realizar tareas complejas y repetitivas. <br>
+                                </p>
 
-</p>
-                            
+                            </div>
+
                         </div>
-                       
                     </div>
-                </div>
 
-                   
+                    <div class="col-md-4 feature-gird">
+                        <div class="row features-hny-inner-gd">
+                            <div class="col-md-3 featured_grid_left">
+                                <div class="icon_left_grid">
+                                    <p class="text-center"><img class="img-fluid" style="max-height: 200px" src="/images/solucionesGeomatica/servicios/ecopia/tres.PNG" alt=""></p>
+                                </div>
+                            </div>
+                            <div class="col-md-9 featured_grid_right_info pl-lg-0">
+                                <h4>Actualizable</h4>
+                                <p>Ecopia cuenta con una amplia fuente de datos para producir mapas vectoriales en HD que representan la versión mas actualizada de la realidad</p>
+
+                            </div>
+
+                        </div>
+                    </div>
+
+
+
+
+
+                    
                 </div>
             </div>
-        </div>
-    </section>
-    <!-- //single -->
+</section>
+<!-- //single -->
 
 @endsection
